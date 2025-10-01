e0fae55c941570c14be2f165321b60559e0642c8
Scott Walker <scott@swlt.ru>
2025-10-02 01:03:02 +0300

    Добавлен новый файл main.ts, который инициализирует приложение, создает контекст и монтирует его на элемент с id "app".

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
+import "./assets/main.css"
+
 import Application from "@base/Application"
 import Context from "@base/Context"
-import App from "./App"
-import config from "@config"
+import App from "./App.vue"
+import { config } from "@config"
 import utils from "@utils"
 import plugins from "@plugins"
 import modules from "@modules"
 
 // Создать контекст приложения
-const context = new Context(config, { dependencies: utils, plugins })
+const context = new Context(config, { dependencies: utils({ config: config.utils }), plugins })
 
 // Инициализировать приложение
 const app = new Application(App, modules, context, config.main)

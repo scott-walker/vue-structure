68724fbf67d2da4c48605cd0c8f8b954e498abe4
Scott Walker <scott@swlt.ru>
2025-10-03 20:56:05 +0300

    Обновлен компонент App.vue: удалены устаревшие компоненты AppNavigation и AppContent, добавлен компонент router-view для управления маршрутизацией, улучшена структура кода с использованием UserPanel.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
+<script setup lang="ts">
+import UserPanel from "@modules/user/components/UserPanel.vue"
+</script>
+
 <template>
   <div class="application">
     <UserPanel />
-    <AppNavigation />
-    <AppContent />
+    <router-view />
   </div>
 </template>
-
-<script setup lang="ts">
-import UserPanel from "@/components/UserPanel.vue"
-import AppNavigation from "@/components/AppNavigation.vue"
-import AppContent from "@/components/AppContent.vue"
-</script>
 
 <style>
 .application {

953217997d1dfe0e3c5fb5af115fd88717540342
Scott Walker <scott@swlt.ru>
2025-10-03 23:03:40 +0300

    Добавлен новый компонент Login.vue с шаблоном и стилями для страницы входа, а также обновлен компонент User.vue для отображения имени пользователя и перенаправления на страницу входа при отсутствии авторизации, что улучшает пользовательский опыт и навигацию в приложении.

---
+++ src/modules/user/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <p>Please login to continue</p>
+  </div>
+</template>
+
+<style scoped>
+.login {
+  margin: 20px;
+}
+</style>
--- src/modules/user/views/User.vue
+++ src/modules/user/views/User.vue
@@ -1,6 +1,25 @@
+<script setup lang="ts">
+import { computed, inject, watch } from "vue"
+import { type SharedContext } from "@types"
+import { type UserStore } from "../types"
+
+const { useStored, useRouter } = inject<SharedContext>("context")!
+const userStore = useStored<UserStore>("user")
+const router = useRouter()
+
+const isLogged = computed(() => userStore.isLogged)
+const name = computed(() => userStore.name)
+
+watch(isLogged, () => {
+  if (!isLogged.value) {
+    router.push({ name: "Login" })
+  }
+})
+</script>
+
 <template>
   <div class="user">
-    <h1>User</h1>
+    <h1>User: {{ name }}</h1>
     <p>This is user page</p>
   </div>
 </template>

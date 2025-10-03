a5a9362476e1e40d249e5d184564882fbbe49668
Scott Walker <scott@swlt.ru>
2025-10-03 23:03:03 +0300

    Добавлены новые компоненты NotFound.vue и PermissionDenied.vue для обработки ошибок 404 и отказа в доступе с соответствующими шаблонами и стилями, что улучшает пользовательский интерфейс и навигацию приложения.

---
+++ src/modules/base/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="not-found">
+    <h1>NotFound (404)</h1>
+    <p>Page not found</p>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  margin: 20px;
+}
+</style>
---
+++ src/modules/base/views/PermissionDenied.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="permission-denied">
+    <h1>PermissionDenied</h1>
+    <p>You are not authorized to access this page</p>
+  </div>
+</template>
+
+<style scoped>
+.permission-denied {
+  margin: 20px;
+}
+</style>

7fe7f833638cacdb3effdbabe57203a17f6ea9b4
Scott Walker <scott@swlt.ru>
2025-10-03 23:05:24 +0300

    Обновлены типы инициализации в модулях user: изменен тип возвращаемого значения функции initShared на ModuleInitShared и добавлен тип для функции initStored, что улучшает типизацию и согласованность кода.

--- src/modules/user/share.ts
+++ src/modules/user/share.ts
@@ -1,10 +1,10 @@
-import type { SharedApi, IModule, UtilsApi } from "@types"
+import type { SharedApi, ModuleInitShared, UtilsApi } from "@types"
 import { UserService } from "./services/UserService"
 
 /**
  * Общедоступный API модуля "user"
  */
-export const initShared: IModule["initShared"] = ({ useShared }): SharedApi => {
+export const initShared: ModuleInitShared = ({ useShared }): SharedApi => {
   const { httpClient, localStorage } = useShared<UtilsApi>("utils")
   const userService = new UserService(httpClient, localStorage)
 
--- src/modules/user/store.ts
+++ src/modules/user/store.ts
@@ -1,10 +1,11 @@
 import { ref } from "vue"
+import type { ModuleInitStored } from "@types"
 import type { UserStore } from "./types"
 
 /**
  * Хранилище состояния модуля "user"
  */
-export const initStored = (): UserStore => {
+export const initStored: ModuleInitStored<UserStore> = () => {
   const name = ref("")
   const isLogged = ref(false)
   const loading = ref(false)

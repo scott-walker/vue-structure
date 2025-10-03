5e007af11e9de15638926c85df9e6c16fac081b5
Scott Walker <scott@swlt.ru>
2025-10-03 20:54:43 +0300

    Удалены устаревшие файлы модуля base: depends.ts, index.ts, routes.ts, store.ts, services/UserService.ts, store/app.ts, store/index.ts, store/user.ts и views/Home.vue, содержащие неиспользуемый код и зависимости.

--- src/modules/base/index.ts
+++
@@ -1,6 +0,0 @@
-import Module from "@base/Module"
-import routes from "./routes"
-import store from "./store"
-import dependencies from "./depends"
-
-export default new Module("base", { routes, store, dependencies })

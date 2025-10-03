85092311d08affee3f99caef2726d19ce2540fb9
Scott Walker <scott@swlt.ru>
2025-10-03 22:34:55 +0300

    Добавлен новый компонент Button.vue: реализован шаблон кнопки с поддержкой классов и стилизацией для улучшения пользовательского интерфейса.

---
+++ src/shared/ui/Button.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+  class?: string
+}>()
+</script>
+
+<template>
+  <button class="button" :class="props.class">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  padding: 8px 16px;
+  background: var(--vt-c-primary);
+  color: var(--vt-c-white);
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
